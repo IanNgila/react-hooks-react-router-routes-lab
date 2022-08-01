2310e164c8db66d12c8a231aef81629ec0119fe9
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-08-01 10:40:41 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,6 +3,27 @@ import { actors } from "../data";
 
 function Actors() {
   return <div>{/*{code here}*/}</div>;
+
 }
+const Actors = () => {
+  const actorItems =actors.map((actor) => (
+    <div key={actor.name}>
+      <h2>actor.name</h2>
+      <ul>
+        {actor.movies.map((movie) =>
+        (<li key={movie}>
+          {movie}
+        </li>))}
+      </ul>
+    </div>
+  ))
+}
+
+return (
+  <div>
+    <h1>Actors page</h1>
+    {actorItems}
+  </div>
+)
 
 export default Actors;
